f797c577fbae5fcf96e53f468e70e3cab7854471
Florent Moreau <florent.cel@gmail.com>
2018-05-13 20:55:53 +0200

    active record model product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,10 @@
 class ProductsController < ApplicationController
-  PRODUCTS = [
-    {name: "Kudoz", category: "pro"},
-    {name: "uSlide", category: "education"},
-    {name: "Hubum", category: "lifestyle"},
-    {name: "Roadstr", category: "lifestyle"},
-  ]
 
   def index
-    if params[:filter]
-      @products = PRODUCTS.select {|product| product[:category] == params[:filter]}
-    else
-      @products = PRODUCTS
-    end
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
