12443d09e106465cb04c67bf33a91db54b6c8668
Florent Moreau <florent.cel@gmail.com>
2018-05-15 22:22:18 +0200

    add resource for product and CRUD actions for products

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,25 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/team" => "pages#team"
-  get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
+
+  resources :products
+
+  # get "/team" => "pages#team"
+  # get "/contact" => "pages#join_us"
+
+  # # read routes
+  # get "/products" => "products#index"
+  # get "/products/:id" => "products#show"
+
+  # # create routes
+  # get "/products/new" => "products#new"
+  # post "products" => "products#create"
+
+  # # update routes
+  # get "/products/:id/edit" => "products#edit"
+  # patch "products/:id" => "products#update"
+
+  # # delete routes
+  # delete "/products/:id" => "products#destroy"
+  # patch "products/:id" => "products#update"
 end
