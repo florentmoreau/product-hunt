c73566a4651aeec8f51c8b3f2748fc9e99555631
Florent Moreau <florent.cel@gmail.com>
2018-05-21 15:53:48 +0200

    Add function destroy and refactor form and find params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,15 @@
 class ProductsController < ApplicationController
 
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
-
   def show
-    @product = Product.find(params[:id])
   end
-
   def new
     @product = Product.new
   end
-
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -20,13 +18,9 @@ class ProductsController < ApplicationController
       render :new
     end
   end
-
   def edit
-    @product = Product.find(params[:id])
   end
-
   def update
-    @product = Product.find(params[:id])
    if @product.update(product_params)
     redirect_to products_path
    else
@@ -35,12 +29,15 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-
+    @product.destroy
+    redirect_to products_path
   end
 
   private
-
   def product_params
     params.require(:product).permit(:name, :url)
+  end
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
