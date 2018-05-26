49461ec0085adb9ad8ca52c565c7b4a75aaf9ae8
Florent Moreau <florent.cel@gmail.com>
2018-05-26 18:28:02 +0200

    add category to products and implement seach by category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_185832) do
+ActiveRecord::Schema.define(version: 2018_05_22_201408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_05_13_185832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "tagline"
+    t.string "category"
   end
 
 end
